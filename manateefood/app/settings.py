755c3d09d0b90ed91c54c679f5bde52afd1ff482
Timothée Lottaz <timozattol@gmail.com>
2017-06-24 01:15:09 +0200

    Create Product Model

--- manateefood/app/settings.py
+++ manateefood/app/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'djangobower',
-    
-    'main',
+
+    'main', 'products'
 ]
 
 MIDDLEWARE = [
--- manateefood/products/models.py
+++ manateefood/products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+#from users.models import House
+
+class ProductCategory(models.Model):
+    category_name = models.CharField(_("product name"), max_length=100, blank=False)
+
+class Product(models.Model):
+    product_name = models.CharField(_("product name"), max_length=100, blank=False)
+    product_price = models.IntegerField(_("product price"), default=0, blank=False)
+
+    #product_house = models.ForeignKey(House, on_delete=models.CASCADE)
+    product_category = models.ForeignKey(ProductCategory, null=True, on_delete=models.SET_NULL)
+
+    created_at = models.DateTimeField(_("datetime created"), auto_now=True)
+    updated_at = models.DateTimeField(_("datetime updated"), auto_now=True)
+    deleted_at = models.DateTimeField(_("datetime deleted"), default=None)
